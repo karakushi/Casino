6443ced03f9b08c593a640c73cf87c10d954c43d
Patrick Blattmann <89085636+buDget01@users.noreply.github.com>
2023-05-11 22:36:36 +0200

    Update script.js
    Nach dem Klicken auf den "Drehen"-Button drehen sich die Elemente problemlos. Die Drei Slots haben beim Spinnen verschiedene Elemente angezeigt und wenn die Animationen fertig sind, weiss ich, ob ich gewonnen habe oder nicht.

---
+++ script.js
@@ -0,0 +1,96 @@
+const symbols = ['🍎', '🍇', '🍉'];
+const slot1 = document.getElementById('slot1');
+const slot2 = document.getElementById('slot2');
+const slot3 = document.getElementById('slot3');
+
+function fillSlot(slot) {
+    const inner = slot.querySelector('.slot-inner');
+    inner.innerHTML = symbols.join('') + symbols[0];
+    inner.style.transform = `translateY(${-2.5 * (symbols.length - 1)}rem)`;
+}
+
+async function spinSlot(slot) {
+    return new Promise((resolve) => {
+        const inner = slot.querySelector('.slot-inner');
+        const randomIndex = Math.floor(Math.random() * symbols.length);
+        const targetPosition = -2.5 * randomIndex;
+
+        let spinCount = 0;
+        const spin = () => {
+            setTimeout(() => {
+                inner.style.transform = `translateY(${parseFloat(inner.style.transform.slice(11)) - 2.5}rem)`;
+                if (parseFloat(inner.style.transform.slice(11)) <= -2.5 * (symbols.length)) {
+                    inner.style.transform = `translateY(0rem)`;
+                }
+
+                spinCount++;
+                if (spinCount < symbols.length * 3 + randomIndex) {
+                    spin();
+                } else {
+                    inner.style.transform = `translateY(${targetPosition}rem)`;
+                    resolve(symbols[randomIndex]);
+                }
+            }, 100);
+        };
+
+        spin();
+    });
+}
+
+
+
+async function spin() {
+    let tokens = parseFloat(localStorage.getItem('tokens')) || 0;
+
+    if (tokens < 5) {
+        alert('Du hast nicht genug Tokens.');
+        return;
+    }
+
+    tokens -= 5;
+    localStorage.setItem('tokens', tokens);
+
+    const results = await Promise.all([
+        spinSlot(slot1),
+        spinSlot(slot2),
+        spinSlot(slot3),
+    ]);
+
+    const [result1, result2, result3] = results;
+
+    const messageElement = document.getElementById('message');
+    const combination = `${result1}${result2}${result3}`;
+
+    const winnings = calculateWinnings(combination);
+    tokens += winnings;
+    localStorage.setItem('tokens', tokens);
+    tokensElement.textContent = `Tokens: ${tokens.toFixed(2)}`;
+
+    if (winnings > 0) {
+        messageElement.textContent = `Glückwunsch! Du hast ${winnings} Tokens gewonnen!`;
+    } else {
+        messageElement.textContent = '';
+    }
+}
+
+
+const tokensElement = document.getElementById('tokens');
+const tokens = parseFloat(localStorage.getItem('tokens')) || 0;
+tokensElement.textContent = `Tokens: ${tokens.toFixed(2)}`;
+
+fillSlot(slot1);
+fillSlot(slot2);
+fillSlot(slot3);
+
+function calculateWinnings(combination) {
+    switch (combination) {
+        case '🍎🍎🍎':
+            return 20;
+        case '🍇🍇🍇':
+            return 15;
+        case '🍉🍉🍉':
+            return 10;
+        default:
+            return 0;
+    }
+}
